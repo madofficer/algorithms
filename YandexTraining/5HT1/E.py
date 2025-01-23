7405de4ec82c2d3fba86741c21d9b436ba1afaf3
madofficer <127042076+madofficer@users.noreply.github.com>
2025-01-23 18:28:37 +0300

    delete 1 mort useless dir

--- YandexTraining/5HT1/E.py
+++
@@ -1,8 +0,0 @@
-n, k, d = [int(x) for x in input().split()]
-
-for i in range(10):
-    if (10 * n + i) % k == 0:
-        print(str(10 * n + i) + '0' * (d - 1))
-        break
-else:
-    print(-1)
